7e4c4d931a66a7b1cc32463b294ff9010c714621
Shueb Dalvi <shueb123@gmail.com>
2021-09-18 22:06:06 +0530

    initial commit

---
+++ src/test/java/com/shuebdalvi/registry/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuebdalvi.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
